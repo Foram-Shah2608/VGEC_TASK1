5f6f7cb255a3e6bbd2304f3c2d86cf06054065a0
Foram-Shah2608 <161506192+Foram-Shah2608@users.noreply.github.com>
2024-02-28 08:58:21 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+	<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE-edge">
+		<meta name="viewport" content="width-device-width,initial-scale=1.0">
+		<title> Home Page</title>
+
+<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+			
+<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="css/linearicons.css">=
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link rel="stylesheet" href="css/nice-select.css">
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/main.css">
+			<link rel="stylesheet" href="css/style.css">
+		</head>
+		<body>
+
+			<!-- Start Header Area -->
+			<header class="default-header">
+				<nav class="navbar navbar-expand-lg  navbar-light bg-white">
+					<div class="container"  >
+						  <a class="navbar-brand" href="index.php">
+						  	<h3 style="color: black">Student Portal</h3>
+						  </a>
+						  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						    <span class="navbar-toggler-icon"></span>
+						  </button>		 
+ <div class="collapse navbar-collapse justify-content-end align-items-center" id="navbarSupportedContent">					  
+  <ul class="navbar-nav">
+								<li><a href="student/login.php" style="color: black">Student</a></li>
+						    </ul>
+						  </div>						
+					</div>
+				</nav>
+			</header>
+			<!-- End Header Area -->
+			<!-- start banner Area -->
+			<a href="student/login.php"> <img class="d-block w-100" src="student/img/home.png">	</a>
+			<footer class="footer-area section-gap">
+				<div class="container">
+					<div class="footer-bottom d-flex justify-content-center align-items-center flex-wrap">
+						<h2 class="footer-text" style="color: white">Student Portal </h2><br></div><br>
+						<div class="footer-bottom d-flex justify-content-center align-items-center flex-wrap">
+						<h5 style="color: white">Made By Foram </h3>				
+				</div>
+			</footer>		
+		</body>
+	</html>
